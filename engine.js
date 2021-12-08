9cae039ab5f1a26cde5cef959c3d407f712bbeb6
luiskunz <me@luiskunz.com>
2021-12-08 17:53:17 +0100

    chore: set to uppercase the scope auto

--- engine.js
+++ engine.js
@@ -5,23 +5,23 @@ var map = require('lodash.map');
 var longest = require('longest');
 var chalk = require('chalk');
 
-var filter = function (array) {
-  return array.filter(function (x) {
+var filter = function(array) {
+  return array.filter(function(x) {
     return x;
   });
 };
 
-var headerLength = function (answers) {
+var headerLength = function(answers) {
   return (
     answers.type.length + 2 + (answers.scope ? answers.scope.length + 2 : 0)
   );
 };
 
-var maxSummaryLength = function (options, answers) {
+var maxSummaryLength = function(options, answers) {
   return options.maxHeaderWidth - headerLength(answers);
 };
 
-var filterSubject = function (subject, disableSubjectLowerCase) {
+var filterSubject = function(subject, disableSubjectLowerCase) {
   subject = subject.trim();
   if (
     !disableSubjectLowerCase &&
@@ -39,14 +39,14 @@ var filterSubject = function (subject, disableSubjectLowerCase) {
 // This can be any kind of SystemJS compatible module.
 // We use Commonjs here, but ES6 or AMD would do just
 // fine.
-module.exports = function (options) {
+module.exports = function(options) {
   var types = options.types;
 
   var length = longest(Object.keys(types)).length + 1;
-  var choices = map(types, function (type, key) {
+  var choices = map(types, function(type, key) {
     return {
       name: (key + ':').padEnd(length) + ' ' + type.description,
-      value: key,
+      value: key
     };
   });
 
@@ -62,7 +62,7 @@ module.exports = function (options) {
     //
     // By default, we'll de-indent your commit
     // template and will keep empty lines.
-    prompter: function (cz, commit) {
+    prompter: function(cz, commit) {
       // Let's ask some questions of the user
       // so that we can populate our commit
       // template.
@@ -76,7 +76,7 @@ module.exports = function (options) {
           name: 'type',
           message: "Select the type of change that you're committing:",
           choices: choices,
-          default: options.defaultType,
+          default: options.defaultType
         },
         {
           type: 'input',
@@ -84,16 +84,16 @@ module.exports = function (options) {
           message:
             'What is the scope of this change (e.g. component or file name): (press enter to skip)',
           default: options.defaultScope,
-          filter: function (value) {
+          filter: function(value) {
             return options.disableScopeLowerCase
               ? value.trim()
-              : value.trim().toLowerCase();
-          },
+              : value.trim().toUpperCase();
+          }
         },
         {
           type: 'input',
           name: 'subject',
-          message: function (answers) {
+          message: function(answers) {
             return (
               'Write a short, imperative tense description of the change (max ' +
               maxSummaryLength(options, answers) +
@@ -101,7 +101,7 @@ module.exports = function (options) {
             );
           },
           default: options.defaultSubject,
-          validate: function (subject, answers) {
+          validate: function(subject, answers) {
             var filteredSubject = filterSubject(
               subject,
               options.disableSubjectLowerCase
@@ -116,7 +116,7 @@ module.exports = function (options) {
                 filteredSubject.length +
                 ' characters.';
           },
-          transformer: function (subject, answers) {
+          transformer: function(subject, answers) {
             var filteredSubject = filterSubject(
               subject,
               options.disableSubjectLowerCase
@@ -127,16 +127,16 @@ module.exports = function (options) {
                 : chalk.red;
             return color('(' + filteredSubject.length + ') ' + subject);
           },
-          filter: function (subject) {
+          filter: function(subject) {
             return filterSubject(subject, options.disableSubjectLowerCase);
-          },
+          }
         },
         {
           type: 'input',
           name: 'body',
           message:
             'Provide a longer description of the change: (press enter to skip)\n',
-          default: options.defaultBody,
+          default: options.defaultBody
         },
         {
           type: 'input',
@@ -144,31 +144,31 @@ module.exports = function (options) {
           default: '-',
           message:
             'A BREAKING CHANGE commit requires a body. Please enter a longer description of the commit itself:\n',
-          when: function (answers) {
+          when: function(answers) {
             return answers.isBreaking && !answers.body;
           },
-          validate: function (breakingBody, answers) {
+          validate: function(breakingBody, answers) {
             return (
               breakingBody.trim().length > 0 ||
               'Body is required for BREAKING CHANGE'
             );
-          },
+          }
         },
         {
           type: 'input',
           name: 'breaking',
           message: 'Describe the breaking changes:\n',
-          when: function (answers) {
+          when: function(answers) {
             return answers.isBreaking;
-          },
-        },
-      ]).then(function (answers) {
+          }
+        }
+      ]).then(function(answers) {
         var wrapOptions = {
           trim: true,
           cut: false,
           newline: '\n',
           indent: '',
-          width: options.maxLineWidth,
+          width: options.maxLineWidth
         };
 
         // parentheses are only needed when a scope is present
@@ -189,6 +189,6 @@ module.exports = function (options) {
 
         commit(filter([head, body, breaking]).join('\n\n'));
       });
-    },
+    }
   };
 };
